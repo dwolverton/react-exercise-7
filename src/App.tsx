a70f2ea5847e86022bba1f00a8553afd4f8d8de7
CTDior <83152946+CTDior@users.noreply.github.com>
2021-09-16 10:24:54 -0400

    add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import MenuList from './components/MenuList';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <MenuList />
+      <Sidebar />
 
     </div>
   );
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,12 +1,20 @@
+import Item from "../models/Item";
 import "./MenuItem.css";
 
 interface Props {
+  dish: Item
 }
 
-function MenuItem({}: Props) {
+function MenuItem({dish}: Props) {
+
   return (
     <li className="MenuItem">
-      
+      <p>Name: {dish.name}</p>
+      <p>Description:{dish.description} </p>
+      <p>Calories: {dish.calories} </p>
+      <p>Price: {dish.price} </p>
+      <p>{dish.vegetarian ? "Vegetarian" : "Not Vegetarian"} </p>
+      <button>Add To Order</button>
     </li>
   );
 }
